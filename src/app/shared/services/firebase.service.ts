db328b8e61807e402dec4dfe680baf7f6e23079c
StevenKDL <s.michelakis@kdevlabs.com>
2025-01-20 14:17:18 +0200

    Change project structure

--- src/app/shared/services/firebase.service.ts
+++ src/app/shared/services/firebase.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FirebaseService {
+  constructor(private firestore: Firestore) {}
 
-  constructor() { }
+  getProducts(): Observable<any[]> {
+    const productsRef = collection(this.firestore, 'products');
+    return collectionData(productsRef, { idField: 'id' }) as Observable<any[]>;
+  }
 }
