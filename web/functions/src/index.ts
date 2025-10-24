b0f8078cf8a37d3a1444d73589199fd781922af7
Stefanos Michelakis <stefanos.michelakis@baseelement.digital>
2025-10-24 20:35:43 +0300

    Stripe error handling on cloud function

--- web/functions/src/index.ts
+++ web/functions/src/index.ts
@@ -12,17 +12,22 @@ interface CreatePaymentIntentRequest {
 
 exports.createPaymentIntent = onCall(
   async (request: CreatePaymentIntentRequest) => {
-    const stripe = new Stripe(process.env.STRIPE_SECRET || '', {
-      apiVersion: '2025-09-30.clover',
-    });
-    const { amount } = request.data;
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount,
-      currency: 'eur',
-      automatic_payment_methods: { enabled: true },
-    });
+    try {
+      const stripe = new Stripe(process.env.STRIPE_SECRET || '', {
+        apiVersion: '2025-09-30.clover',
+      });
+      const { amount } = request.data;
+      console.log('amount: ', amount);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount,
+        currency: 'eur',
+        automatic_payment_methods: { enabled: true },
+      });
 
-    console.log('secret: ', paymentIntent.client_secret);
-    return { clientSecret: paymentIntent.client_secret };
+      return { clientSecret: paymentIntent.client_secret };
+    } catch (error: any) {
+      console.error(error);
+      return { error: error.message };
+    }
   },
 );
